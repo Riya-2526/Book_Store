c4cd43865f9d1864f36126f3597019fb8fcd9164
Riya-2526 <138708104+Riya-2526@users.noreply.github.com>
2024-09-01 15:07:54 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -14,7 +14,7 @@ const app = express();
 app.use(express.json());
 app.use(
   cors({
-    origin: ["http://localhost:5173"],
+    origin: ["https://stellular-monstera-aae9cb.netlify.app/"],
     credentials: true,
   })
 );
