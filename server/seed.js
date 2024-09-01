db57873b9707ba949ba069e71635846ed2fca3c1
Riya-2526 <138708104+Riya-2526@users.noreply.github.com>
2024-09-01 14:41:48 +0530

    Update seed.js

--- server/seed.js
+++ server/seed.js
@@ -1,40 +1,39 @@
-import mongoose from 'mongoose';
-import bcrypt from 'bcrypt';
-import dotenv from 'dotenv';
-import { Admin } from './models/Admin.js';
+import mongoose from "mongoose";
+import bcrypt from "bcryptjs";
+import dotenv from "dotenv";
+import { Admin } from "./models/Admin.js";
 
 dotenv.config(); // Ensure environment variables are loaded
 
 async function AdminAccount() {
   try {
     // Connect to MongoDB
-    const mongoURI = process.env.MONGO_URI;
-    if (!mongoURI) {
-      throw new Error('MONGO_URI is not defined in .env file');
-    }
-    
-    await mongoose.connect(mongoURI, { useNewUrlParser: true, useUnifiedTopology: true });
+    await mongoose.connect(process.env.MONGO_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
 
     // Check if any admin accounts already exist
     const adminCount = await Admin.countDocuments();
 
     if (adminCount === 0) {
       // Hash the password
-      const hashedPassword = await bcrypt.hash('adminpassword', 10);
+      const hashedPassword = await bcrypt.hash("adminpassword", 10);
 
       // Create a new admin instance
       const newAdmin = new Admin({
-        username: 'admin',
+        username: "admin",
         password: hashedPassword,
       });
 
       // Save the new admin account to the database
+
       await newAdmin.save();
       console.log("Admin account created successfully");
     } else {
       console.log("Admin account already exists");
     }
-    
+
     // Disconnect from MongoDB
     await mongoose.disconnect();
   } catch (err) {
